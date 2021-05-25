90a571390cec8add4c24126b456a77907e4cf4e1
Gaston <gaston.diaz.contractor@macmillan.com>
2021-05-24 21:06:50 -0300

    Pequeños cambios

--- src/main/java/dan/tp2021/cuentacorriente/services/PagoServiceImpl.java
+++ src/main/java/dan/tp2021/cuentacorriente/services/PagoServiceImpl.java
@@ -1,14 +1,13 @@
 package dan.tp2021.cuentacorriente.services;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import dan.tp2021.cuentacorriente.dao.PagoRepositoryH2;
+import dan.tp2021.cuentacorriente.dao.PagoRepository;
 import dan.tp2021.cuentacorriente.domain.Pago;
 import dan.tp2021.cuentacorriente.exceptions.pago.PagoException;
 import dan.tp2021.cuentacorriente.exceptions.pago.PagoNotFoundException;
@@ -19,8 +18,7 @@ public class PagoServiceImpl implements PagoService {
 	private static final Logger logger = LoggerFactory.getLogger(PagoServiceImpl.class);
 
 	@Autowired
-//	PagoInMemoryRepository inMemoryRepository;
-	PagoRepositoryH2 inMemoryRepository;
+	PagoRepository pagoRepository;
 
 	@Override
 	public Pago savePago(Pago p) throws PagoException {
@@ -29,9 +27,9 @@ public class PagoServiceImpl implements PagoService {
 			// Si quiere actualizar un pago, primero me fijo si dicho pago existe mediante
 			// su id.
 			logger.debug("Id no nulo, quiere actualizarlo.");
-			if (inMemoryRepository.existsById(p.getId())) {
+			if (pagoRepository.existsById(p.getId())) {
 				try {
-					return inMemoryRepository.save(p);
+					return pagoRepository.save(p);
 				} catch (Exception e) {
 					logger.error("Error al actualizar pago: " + e.getMessage());
 					throw new PagoException("Error al guardar pago actualizado");
@@ -47,7 +45,7 @@ public class PagoServiceImpl implements PagoService {
 				//TODO es posible que se pueda crear un cliente en este microservicio? o siempre lo tengo que gestionar con el microservicio de USUARIOS?
 				p.setFechaPago(Instant.now());
 				logger.debug("Entro a save pago, fechaPago= " + p.getFechaPago());
-				return inMemoryRepository.save(p);
+				return pagoRepository.save(p);
 			} catch (Exception e) {
 				logger.error("Error al guardar el pago: " + e.getMessage());
 				throw new PagoException("Error al guardar el pago: " + e.getMessage());
@@ -60,9 +58,9 @@ public class PagoServiceImpl implements PagoService {
 	public Pago deletePagoById(Integer idPago) throws PagoException {
 
 		try {
-			Optional<Pago> p = inMemoryRepository.findById(idPago);
+			Optional<Pago> p = pagoRepository.findById(idPago);
 			if (p.isPresent()) {
-				inMemoryRepository.deleteById(idPago);
+				pagoRepository.deleteById(idPago);
 				return p.get();
 			}
 		} catch (Exception e) {
@@ -80,7 +78,7 @@ public class PagoServiceImpl implements PagoService {
 	@Override
 	public Pago getPagoById(Integer idPago) throws PagoException {
 
-		Optional<Pago> result = inMemoryRepository.findById(idPago);
+		Optional<Pago> result = pagoRepository.findById(idPago);
 		if (result.isPresent()) {
 			return result.get();
 		}
@@ -90,9 +88,9 @@ public class PagoServiceImpl implements PagoService {
 
 	@Override
 	public List<Pago> getListaPagos() throws PagoException {
-		List<Pago> resultado = new ArrayList<>();
+		List<Pago> resultado;
 		try {
-			inMemoryRepository.findAll().forEach(p -> resultado.add(p));
+			resultado = pagoRepository.findAll();
 		} catch (Exception e) {
 			logger.error("Error al obtener pagos. Mensaje error: " + e.getMessage());
 			throw new PagoException("Error al obtener la lista de pagos: " + e.getMessage());
@@ -102,10 +100,10 @@ public class PagoServiceImpl implements PagoService {
 
 	@Override
 	public List<Pago> getListaPagosByIdCliente(Integer idCliente) throws PagoException {
-		List<Pago> resultado = new ArrayList<>();
+		List<Pago> resultado;
 		try {
 //			inMemoryRepository.findAll().forEach(p -> {if(p.getCliente().getId().equals(idCliente)) resultado.add(p);});
-			resultado = inMemoryRepository.findByIdCliente(idCliente);
+			resultado = pagoRepository.findByClienteId(idCliente);
 		} catch (Exception e) {
 			logger.error("Error al obtener pagos por id clientes. Mensaje: " + e.getMessage());
 			throw new PagoException(
@@ -116,10 +114,10 @@ public class PagoServiceImpl implements PagoService {
 
 	@Override
 	public List<Pago> getListaPagosByCuitCliente(String cuit) throws PagoException {
-		List<Pago> resultado = new ArrayList<>();
+		List<Pago> resultado;
 		try {
 //			inMemoryRepository.findAll().forEach(p -> {if(p.getCliente().getCuit().equals(cuit)) resultado.add(p);});
-			resultado = inMemoryRepository.findByCuitCliente(cuit);
+			resultado = pagoRepository.findByClienteCuit(cuit);
 		} catch (Exception e) {
 			logger.error("Error al buscar pagos por cuit de cliente. Mensaje de error: " + e.getMessage());
 			throw new PagoException(
@@ -129,11 +127,11 @@ public class PagoServiceImpl implements PagoService {
 	}
 
 	@Override
-	public List<Pago> getPagosByParams(Integer id, String cuit) throws PagoNotFoundException, PagoException {
+	public List<Pago> getPagosByParams(Integer id, String cuit) throws PagoException {
 
 		if (id > 0 && !cuit.isBlank()) {
 			try {
-				return inMemoryRepository.getPagosByCuitOrIdCliente(id, cuit);
+				return pagoRepository.findByClienteIdOrClienteCuit(id, cuit);
 			} catch (Exception e) {
 				logger.error("Error al obtener pagos desde la BD. Mensaje: " + e.getMessage());
 				throw new PagoException("Error al obtener pagos desde la BD");
